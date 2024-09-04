1a7feb3c06497705a20fa38d4e665f514afffae8
jakecolacoo <94580156+jakecolacoo@users.noreply.github.com>
2024-09-03 22:19:59 -0600

    enemy and health

---
+++ Assets/Scripts/health/enemyDoDamage.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class doDamage : MonoBehaviour
+{
+
+    public int damage;
+    public health playerHealth;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
