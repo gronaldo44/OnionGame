cab6cc5b65424693b0db375d70bec9364e528e9a
jakecolacoo <94580156+jakecolacoo@users.noreply.github.com>
2024-11-29 16:09:42 -0700

    Added fox cub logic and new text box

--- Assets/Scripts/Environment/SceneTrigger.cs
+++ Assets/Scripts/Environment/SceneTrigger.cs
@@ -6,22 +6,6 @@ public class SceneTrigger : MonoBehaviour
 {
     [SerializeField] private string sceneDestination;
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.TryGetComponent<PlayerController>(out var player))
-    //    {
-    //        Debug.Log("Triggered Scene Change: " + sceneDestination);
-    //        if (GameManager.Instance != null)
-    //        {
-    //            GameManager.Instance.ChangeScene(sceneDestination);
-    //        }
-    //        else
-    //        {
-    //            Debug.LogError("GameManager instance is null. Cannot change scene.");
-    //        }
-    //    }
-    //}
-
     void Update()
     {
         if (GameObject.FindGameObjectsWithTag("DestroyablePlant").Length == 0)
