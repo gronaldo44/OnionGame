d847d285ef0d1fd711a3611d1aa4a7b879ff0b9d
jakecolacoo <94580156+jakecolacoo@users.noreply.github.com>
2024-12-04 00:53:49 -0700

    Added end screen buttons

---
+++ Assets/Scripts/EndScreenManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EndScreenManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(GameObject.Find("MainMenu"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnQuitGame()
+    {
+        Debug.Log("Exiting Game");
+#if UNITY_EDITOR
+        // If running in the Unity Editor, stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // If running in a build, quit the application
+        Application.Quit();
+#endif
+    }
+}
