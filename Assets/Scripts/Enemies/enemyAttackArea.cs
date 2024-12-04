65c2a525223ea95b58318b25bfd7c2e0711e4fd1
jakecolacoo <94580156+jakecolacoo@users.noreply.github.com>
2024-12-03 21:22:57 -0700

    fixed health icon issues

--- Assets/Scripts/Enemies/enemyAttackArea.cs
+++ Assets/Scripts/Enemies/enemyAttackArea.cs
@@ -2,14 +2,41 @@ using UnityEngine;
 
 public class EnemyAttackArea : MonoBehaviour
 {
-    [SerializeField] public int dmg;
+    private int dmg = 1;
+    private float cooldown = 0.05f;
+    private float lastTriggerTime = 0f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<PlayerDoDamage>() != null)
+        if (Time.time - lastTriggerTime > cooldown)
         {
-            PlayerHealth h = collision.GetComponent<PlayerHealth>();
-            h.TakeDamage(dmg);
+            //handle kb
+            Rigidbody2D playerRb = collision.GetComponent<Rigidbody2D>();
+            if (playerRb != null)
+            {
+                Vector2 kbDirection;
+
+                //knock left
+                if (collision.transform.position.x > transform.position.x)
+                {
+                    kbDirection = new Vector2(75f, 10f);
+                }
+                else //knock right
+                {
+                    kbDirection = new Vector2(-75f, 10f);
+                }
+
+                playerRb.AddForce(kbDirection, ForceMode2D.Impulse);
+            }
+
+            lastTriggerTime = Time.time;
+
+            //handle damage
+            if (collision.GetComponent<PlayerDoDamage>() != null)
+            {
+                PlayerHealth h = collision.GetComponent<PlayerHealth>();
+                h.TakeDamage(dmg);
+            }
         }
     }
 }
